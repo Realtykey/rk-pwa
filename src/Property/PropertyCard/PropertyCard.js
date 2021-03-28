5cac492bfd9b6e43c6aed165abb415eb921eb014
Ronny Cajas <ronny.cajas@crifa.com>
2021-03-28 18:08:16 -0500

    removes map capture from request

--- src/Property/PropertyCard/PropertyCard.js
+++ src/Property/PropertyCard/PropertyCard.js
@@ -25,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.custom,
     fontSize: '14px',
     maxWidth: window.screen.width < 600 ? window.screen.width : 600,
+    minWidth: window.screen.width < 600 ? window.screen.width : 600,
     transition: "0.3s",
     boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
     "&:hover": {
@@ -82,11 +83,7 @@ export default function PropertyCard({hit}) {
         <ImagesPreview urls={hit.photos} />
       </CardContent>
       :
-      <CardMedia
-        className={classes.media}
-        image={hit.map.snapUrl}
-        title="Paella dish"
-      />
+      <></>
       }
 
       <CardActions >
