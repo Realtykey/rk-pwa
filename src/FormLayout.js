c7f1668ab64cc9ebad9ec0163b37d1ec5a908406
Ronny Cajas <ronny.cajas@epn.edu.ec>
2021-04-09 18:17:43 -0500

    css scroll disabled on forms

--- src/FormLayout.js
+++ src/FormLayout.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: '20px 20px 0 20px',
-        overflow: 'scroll',
         whiteSpace: 'nowrap',
         paddingTop: 100,
         paddingBottom: 30,
