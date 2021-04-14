236deec4eeef044e13a3afe8be6976adbd9692a1
Ronny Cajas <ronny.cajas@epn.edu.ec>
2021-04-14 18:22:05 -0500

    eslint

--- src/Property/PropertyCard/ProfileAvatar.js
+++ src/Property/PropertyCard/ProfileAvatar.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles'
+import Avatar from '@material-ui/core/Avatar'
 
-import Chip from '@material-ui/core/Chip';
+import Chip from '@material-ui/core/Chip'
 
-//database
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import '../../Property/PropertyCard/styles.css'
-//redux imports
 import { useDispatch } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    color:'white !important',
+    color: 'white !important',
     padding: '20px 10px',
     width: '100%',
     backgroundColor: theme.palette.background.paper,
@@ -23,105 +22,111 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column'
   },
   inline: {
-    display: 'inline',
+    display: 'inline'
   },
   identity: {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   photo: {
-
     paddingRight: '10px',
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     align: 'center',
-    justify: 'center',
-
+    justify: 'center'
   },
   large: {
     width: theme.spacing(7),
-    height: theme.spacing(7),
+    height: theme.spacing(7)
   },
 
   placeholder: {
     color: 'gray'
   }
-
-}
-));
+}))
 
 const premium = {
   backgroundColor: 'rgb(0	190	93)',
   color: 'white',
   fontWeight: 'bold',
-  letterSpacing: '1px',
+  letterSpacing: '1px'
 }
 
 const member = {
   backgroundColor: 'rgb(0	110	213	)',
   color: 'white',
   fontWeight: 'bold',
-  letterSpacing: '1px',
+  letterSpacing: '1px'
 }
 
-export default function ProfileAvatar(props) {
-  const dispatch = useDispatch();
-  const db = firebase.firestore();
-  const uid = props.uid;
-  const classes = useStyles();
+export default function ProfileAvatar (props) {
+  const dispatch = useDispatch()
+  const db = firebase.firestore()
+  const { uid } = props
+  const classes = useStyles()
 
-  const [data,setData] = useState(null);
+  const [data, setData] = useState(null)
 
-  const setUserPreview = userPreview => dispatch({ type: 'USER_PREVIEW', payload: userPreview });
+  const setUserPreview = (userPreview) =>
+    dispatch({ type: 'USER_PREVIEW', payload: userPreview })
 
   const renderData = () => {
-
     return (
-      <a className={classes.root} onClick={() => setUserPreview({...data})}>
-
+      <a className={classes.root} onClick={() => setUserPreview({ ...data })}>
         <div className={classes.identity}>
-
           <div style={{ display: 'flex', width: '80%' }}>
             <div className={classes.photo}>
               <Avatar src={data.photoUrl} className={classes.large} />
             </div>
 
             <div className={classes.info}>
-              <div>{data.name} {data.lname}</div>
-              <div className={classes.placeholder} > {data.role}</div>
-
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                {' '}
+                <div style={{ color: 'gray', marginRight: 5 }}>CBR </div>{' '}
+                <div>
+                  {' '}
+                  {data.name} {data.lname}
+                </div>
+              </div>
+              {
+                <div className={classes.placeholder}>
+                  {data.role} con licencia
+                </div>
+              }
             </div>
           </div>
 
-          <div style={{display:'flex', width: '20%' }}>
+          <div style={{ display: 'flex', width: '20%' }}>
             <Chip
-              classes={{root:'root'}}
+              classes={{ root: 'root' }}
               label={data.status}
-              style={data.status == 'Miembro' ? member : premium}
+              style={data.status === 'Miembro' ? member : premium}
             />
           </div>
-
         </div>
-
       </a>
     )
   }
 
-  useEffect(
-    () => {
-      if (props.uid) {
-        db.collection('users').doc(uid).get().then(
-          (userDoc) => {
-            setData(userDoc.data());
-          }
-        )
-      }
+  useEffect(() => {
+    if (uid) {
+      db.collection('users')
+        .doc(uid)
+        .get()
+        .then((userDoc) => {
+          setData(userDoc.data())
+        })
     }
-    , [uid]);
+  }, [uid])
 
+  return data ? renderData() : <div></div>
+}
 
-  return (
-    data ? renderData() : <div></div>
-  );
+ProfileAvatar.propTypes = {
+  uid: PropTypes.string
+}
+
+ProfileAvatar.defaultProps = {
+  uid: ''
 }
