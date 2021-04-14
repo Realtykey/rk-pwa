61db17df7463bd56e8b51cfe4b5db0b3a52ddf3d
Ronny Cajas <ronny.cajas@epn.edu.ec>
2021-04-14 15:58:56 -0500

    eslint

--- src/AgentCard/AgentCard.js
+++ src/AgentCard/AgentCard.js
@@ -1,100 +1,99 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import Button from '@material-ui/core/Button';
-
-import HomeWorkIcon from '@material-ui/icons/HomeWork';
-import Grid from '@material-ui/core/Grid';
-import '../App.css'
-//chip avatares
-import Chip from '@material-ui/core/Chip';
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Chip from '@material-ui/core/Chip'
 import UserComments from './UserComments'
+import { Rating } from '@material-ui/lab'
+import '../App.css'
 
-//rating
-import { Rating } from '@material-ui/lab';
-
-const useStyles = makeStyles((theme) => ({
-  muicard: {
-    borderRadius: '25px'
-  },
+const useStyles = makeStyles(() => ({
   root: {
-    width: '290px',
-    maxWidth: '290px',
+    width: 290,
+    maxWidth: 290
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-
-  avatar: {
-    backgroundColor: red[500],
+  muicard: {
+    borderRadius: 25
   },
   bigAvatar: {
-    marginTop:'10px',
+    marginTop: 10,
     margin: '0 auto',
     width: 150,
-    height: 150,
+    height: 150
   },
-  outerCircle:{
-    marginTop:'-60px',
-    borderRadius:'100%',
+  outerCircle: {
+    marginTop: -60,
+    borderRadius: '100%',
     margin: '0 auto',
     width: 170,
     height: 170,
-    backgroundColor:'white',
+    backgroundColor: 'white'
   },
-  cardHeader:{
-    color:'white',
+  cardHeader: {
+    color: 'white',
     textAlign: 'center',
-    backgroundColor : 'rgb(0	110	213	)',
-    paddingBottom:'70px'
+    backgroundColor: '#006ED5',
+    paddingBottom: 70
+  },
+  flex: {
+    display: 'flex'
+  },
+  ratingWrapper: {
+    margin: '0 auto'
+  },
+  commentsButton: {
+    color: 'white',
+    backgroundColor: '#4D81ED',
+    width: '100%',
+    margin: '0 auto'
   }
-}));
+}))
 
-export default function AgentCard(props) {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-  const agent = props.agent ? props.agent : null;
+export default function AgentCard (props) {
+  const classes = useStyles()
+  const [expanded, setExpanded] = React.useState(false)
+  const { agent } = props
 
   const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+    setExpanded(!expanded)
+  }
 
   return (
     <div>
       {agent && (
         <Card className={classes.muicard}>
-          {!expanded &&
+          {!expanded && (
             <div className={classes.root}>
               <CardHeader
                 className={classes.cardHeader}
-                title={agent.name + " " + agent.lname}
+                title={agent.name + ' ' + agent.lname}
                 subheader="Quito"
               />
 
-              <div style={{ display: 'flex' }}>
-                <div className = {classes.outerCircle}>
-                  <Avatar alt="No photo" src={agent.photoUrl} className={classes.bigAvatar} />
+              <div className={classes.flex}>
+                <div className={classes.outerCircle}>
+                  <Avatar
+                    alt="No photo"
+                    src={agent.photoUrl}
+                    className={classes.bigAvatar}
+                  />
                 </div>
               </div>
 
-              <CardActions style={{ display: 'flex' }} disableSpacing>
-                <div style={{ margin: '0 auto' }}>
+              <CardActions className={classes.flex} disableSpacing>
+                <div className={classes.ratingWrapper}>
                   <Rating value={agent.score} readOnly />
                 </div>
               </CardActions>
-              <CardContent style={{ display: 'flex' }} >
+              <CardContent className={classes.flex}>
                 <Button
-                  style={{ color: 'white', backgroundColor: 'rgb(77	129	237)', width: '100%', margin: "0 auto" }}
+                  className={classes.commentsButton}
                   variant="outlined"
                   onClick={handleExpandClick}
                 >
@@ -110,63 +109,50 @@ export default function AgentCard(props) {
                   justify="flex-start"
                   alignItems="center"
                 >
-
                   <Grid item>
-
                     <Chip
-                      label={"Nombre " + agent.name + " " + agent.lname}
-
+                      label={'Nombre ' + agent.name + ' ' + agent.lname}
                       variant="outlined"
                     />
-
                   </Grid>
                   <Grid item>
                     <Chip
-                      label={"Licencia " + agent.licenseCode}
-
+                      label={'Licencia ' + agent.licenseCode}
                       variant="outlined"
                     />
-
                   </Grid>
                   <Grid item>
-
-                    <Chip
-                      label={"Celular " + agent.phone}
-
-                      variant="outlined"
-
-                    />
-
+                    <Chip label={'Celular ' + agent.phone} variant="outlined" />
                   </Grid>
                   <Grid item>
-
                     <Chip
-                      label={"Negocios cerrados " + agent.sells}
-
+                      label={'Negocios cerrados ' + agent.sells}
                       variant="outlined"
                     />
-
                   </Grid>
-
                 </Grid>
-
-
               </CardContent>
             </div>
+          )}
 
-          }
-
-          {expanded &&
+          {expanded && (
             <CardContent className={classes.root}>
-              <Button onClick={handleExpandClick} variant="outlined">Volver</Button>
+              <Button onClick={handleExpandClick} variant="outlined">
+                Volver
+              </Button>
               <UserComments userData={agent} />
             </CardContent>
-          }
-
+          )}
         </Card>
-      )
-      }
+      )}
+    </div>
+  )
+}
 
-    </div >
-  );
+AgentCard.propTypes = {
+  agent: PropTypes.object
+}
+
+AgentCard.defaultProps = {
+  agent: null
 }
