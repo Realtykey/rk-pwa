4c1b519eaa492a21206caeb7844ed8bf8b231727
Ronny Cajas <ronny.cajas@epn.edu.ec>
2021-04-14 20:52:23 -0500

    eslint

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,42 +1,36 @@
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import HomeWorkIcon from '@material-ui/icons/HomeWork';
-import { useHistory } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import InputBase from '@material-ui/core/InputBase';
-import Switch from '@material-ui/core/Switch';
+import React, { useState } from 'react'
 
-//auth routing
-import { withRouter } from "react-router";
-import React,{useState} from "react";
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import HomeWorkIcon from '@material-ui/icons/HomeWork'
+import { useHistory, Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import Switch from '@material-ui/core/Switch'
 
-/*ENRUTAMIENTO*/
-import {
-  Link,
-} from "react-router-dom";
-
-function Copyright() {
-  const classes = useStyles();
+function Copyright () {
+  const classes = useStyles()
 
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link className={classes.link} color="inherit" to="https://material-ui.com/">
+      <Link
+        className={classes.link}
+        color="inherit"
+        to="https://material-ui.com/"
+      >
         Valeria
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -44,78 +38,69 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(3)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 2)
   },
   link: {
     color: 'white'
-  },
-
-}));
+  }
+}))
 
 const saveUserDoc = async (userDoc) => {
-  const { app } = await import('./../base');
+  const { app } = await import('./../base')
 
-  const ref = app.firestore().collection('users').doc(userDoc.uid);
+  const ref = app.firestore().collection('users').doc(userDoc.uid)
 
-  return ref.set(userDoc, { merge: true });
-};
+  return ref.set(userDoc, { merge: true })
+}
 
-function SignUp() {
-  const classes = useStyles();
-  const history = useHistory();
-  const { handleSubmit, register } = useForm();
-  const [license,hideLicense] = useState(false);
+function SignUp () {
+  const classes = useStyles()
+  const history = useHistory()
+  const { handleSubmit, register } = useForm()
+  const [license, hideLicense] = useState(false)
 
-  const submit = async data => {
-    const { name, lname, email, password, address, phone } = data;
-    console.log(data);
+  const submit = async (data) => {
+    const { name, lname, email, password, address, phone } = data
     try {
-      //firebase auth
-      const { app } = await import('./../base');
-      await app.auth().createUserWithEmailAndPassword(email, password);
+      const { app } = await import('./../base')
+      await app.auth().createUserWithEmailAndPassword(email, password)
 
       const unsuscribe = app.auth().onAuthStateChanged((user) => {
         if (user) {
-          saveUserDoc(
-            {
-              uid: user.uid,
-              name,
-              lname,
-              email,
-              address,
-              phone,
-              photoUrl: '',
-              experience: 0,
-              licenseCode: "",
-              roles: [],
-              role:"",
-              score: 0,
-              status: "Miembro",
-              sells: 0,
-              ci:"",
-              roles:[]
-            }
-          );
-          unsuscribe();
-          console.log('user created, listener removed');
-          history.push('/Home');
+          saveUserDoc({
+            uid: user.uid,
+            name,
+            lname,
+            email,
+            address,
+            phone,
+            photoUrl: '',
+            experience: 0,
+            licenseCode: '',
+            roles: [],
+            role: '',
+            score: 0,
+            status: 'Miembro',
+            sells: 0,
+            ci: ''
+          })
+          unsuscribe()
+          history.push('/Home')
         }
-      }
-      );
-
+      })
     } catch (error) {
-      alert(error);
+      alert(error)
     }
   }
 
@@ -184,18 +169,23 @@ function SignUp() {
               />
             </Grid>
             <Grid item xs={12}>
-            Tienes licencia ACBIR?
-            <Switch checked={license} onChange={() => hideLicense(!license)} name="checkedB" />
-              {license==true && <TextField
-                inputRef={register({ required: true })}
-                variant="outlined"
-                fullWidth
-                id="licenseCode"
-                label="Reg. Nº"
-                name="licenseCode"
-                autoComplete="licenseCode"
+              Tienes licencia ACBIR?
+              <Switch
+                checked={license}
+                onChange={() => hideLicense(!license)}
+                name="checkedB"
               />
-              }
+              {license === true && (
+                <TextField
+                  inputRef={register({ required: true })}
+                  variant="outlined"
+                  fullWidth
+                  id="licenseCode"
+                  label="Reg. Nº"
+                  name="licenseCode"
+                  autoComplete="licenseCode"
+                />
+              )}
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -219,12 +209,6 @@ function SignUp() {
                 id="password"
               />
             </Grid>
-            <Grid item xs={12}>
-              {/* <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="Recibir noticias y actualizaciones"
-              /> */}
-            </Grid>
           </Grid>
           <Button
             fullWidth
@@ -232,12 +216,13 @@ function SignUp() {
             color="primary"
             type="submit"
             className={classes.submit}
-          >Registrarse</Button>
+          >
+            Registrarse
+          </Button>
           <Grid container justify="flex-end">
-
             <Grid item>
               <Link className={classes.link} to="/SignIn" variant="body2">
-                {"¿Ya tienes una cuenta?"}
+                {'¿Ya tienes una cuenta?'}
               </Link>
             </Grid>
           </Grid>
@@ -247,7 +232,7 @@ function SignUp() {
         <Copyright />
       </Box>
     </Container>
-  );
+  )
 }
 
-export default SignUp;
+export default SignUp
