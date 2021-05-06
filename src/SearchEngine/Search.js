a0ab9b9016fa04454930d30bf353177862f00ce2
Ronny Cajas <ronny1418@hotmail.com>
2021-05-05 19:36:36 -0500

    eslint

--- src/SearchEngine/Search.js
+++ src/SearchEngine/Search.js
@@ -1,72 +1,86 @@
-import React from 'react';
-import algoliasearch from 'algoliasearch/lite';
+import React from 'react'
+import algoliasearch from 'algoliasearch/lite'
 
-import Grid from '@material-ui/core/Grid';
-import './search.css';
-import InfiniteHits from './InfiniteHits';
-import RangeInput from './CustomRangeInput';
-import CustomSearchBox from './CustomSearchBox';
+import Grid from '@material-ui/core/Grid'
+import './search.css'
+import InfiniteHits from './InfiniteHits'
+import RangeInput from './CustomRangeInput'
+import CustomSearchBox from './CustomSearchBox'
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 import {
-    MenuSelect,
-    InstantSearch,
-    Stats,
-    Configure,
-  } from 'react-instantsearch-dom';
+  MenuSelect,
+  InstantSearch,
+  Stats,
+  Configure
+} from 'react-instantsearch-dom'
 
+const searchClient = algoliasearch(
+  'IW55L7EK8V',
+  'e305d8ad0b21a89b620a1e11ab90dbad'
+)
 
-  const searchClient = algoliasearch('IW55L7EK8V', 'e305d8ad0b21a89b620a1e11ab90dbad');
-
-export default function Search({indexName,hitComponent,refinementAttributes,rangeAtributes}) {
-    return (
-        <InstantSearch searchClient={searchClient} indexName={indexName}>
-            <Configure hitsPerPage={2}/>
-            <CustomSearchBox
-                rangeAtributes={rangeAtributes}
-                showLoadingIndicator={true}
-                showReset={false}
-                showSubmit={true}
-                autofocus={true}
-                searchAsYouType={false}
-                translations={{
-                    submitTitle: 'Buscar',
-                    resetTitle: 'Quitar filtros',
-                    placeholder: 'país, cuidad o descripción',
-                }}
-                templates={{
-                    reset: 'reset',
-                }}
-            />
-            <form onSubmit={e => e.preventDefault()} id="ranges-form" className="refinements">
-                <Grid container justify="center">
-                    {rangeAtributes.map(attribute => 
-                        <Grid item xs={12} sm={6} md={3} lg={3} key={uuidv4()}>
-                            <RangeInput               
-                            name={attribute.name}
-                            label={attribute.label}
-                            attribute={attribute.name}
-                            min={1}
-                            precision={0}
-                            />
-                        </Grid>)
-                    }
-                </Grid>
-            </form>   
-            <div className="refinements">
-                {refinementAttributes.map(
-                    attribute => <MenuSelect translations={{seeAllOption:attribute.label}}  key={uuidv4()}  attribute={attribute.name} />
-                )}
-            </div>
-            <Stats translations={
-                {
-                    stats(nbHits, timeSpentMS) {
-                      return `${nbHits} resultados`;
-                    },
-                  }
-            }/>
-            <InfiniteHits hitComponent={hitComponent} minHitsPerPage={16} />
-
-        </InstantSearch>
-    );
+export default function Search ({
+  indexName,
+  hitComponent,
+  refinementAttributes,
+  rangeAtributes
+}) {
+  return (
+    <InstantSearch searchClient={searchClient} indexName={indexName}>
+      <Configure hitsPerPage={2} />
+      <CustomSearchBox
+        rangeAtributes={rangeAtributes}
+        showLoadingIndicator={true}
+        showReset={false}
+        showSubmit={true}
+        autofocus={true}
+        searchAsYouType={false}
+        translations={{
+          submitTitle: 'Buscar',
+          resetTitle: 'Quitar filtros',
+          placeholder: 'país, cuidad o descripción'
+        }}
+        templates={{
+          reset: 'reset'
+        }}
+      />
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        id="ranges-form"
+        className="refinements"
+      >
+        <Grid container justify="center">
+          {rangeAtributes.map((attribute) => (
+            <Grid item xs={12} sm={6} md={3} lg={3} key={uuidv4()}>
+              <RangeInput
+                name={attribute.name}
+                label={attribute.label}
+                attribute={attribute.name}
+                min={1}
+                precision={0}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </form>
+      <div className="refinements">
+        {refinementAttributes.map((attribute) => (
+          <MenuSelect
+            translations={{ seeAllOption: attribute.label }}
+            key={uuidv4()}
+            attribute={attribute.name}
+          />
+        ))}
+      </div>
+      <Stats
+        translations={{
+          stats (nbHits, timeSpentMS) {
+            return `${nbHits} resultados`
+          }
+        }}
+      />
+      <InfiniteHits hitComponent={hitComponent} minHitsPerPage={16} />
+    </InstantSearch>
+  )
 }
