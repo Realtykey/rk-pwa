e7aa4a86d4ff6851ab5df107a63e7fe6ffe78b1d
Ronny Cajas <ronny1418@hotmail.com>
2021-05-28 22:28:20 -0500

    manage profile tests

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,4 +1,4 @@
-import { login, searchUser, register } from '../interactions'
+import { login, searchUser, register,  manageProfile } from '../interactions'
 
 describe(
   'Dev',
@@ -15,9 +15,18 @@ describe(
       cy.visit('http://localhost:3000')
     })
 
-    register()
+    const user = {
+      // email: 'pato1418@yahoo.com',
+      // password: '123456'
 
-    // login('pato1418@yahoo.com', '123456')
+      email: 'kevin@coloma.com',
+      password: '123456'
+    }
+
+    // register()
+
+    login(user.email, user.password)
+
     // searchUser()
   }
 )
