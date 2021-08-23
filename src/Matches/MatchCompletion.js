567301932be558741cb9e98a7838ccfc54c4d102
Ronny Cajas <ronny1418@hotmail.com>
2021-08-22 20:48:31 -0500

    eslint

--- src/Matches/MatchCompletion.js
+++ src/Matches/MatchCompletion.js
@@ -1,160 +1,181 @@
-import React,{ useState } from 'react'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar'
 
-import { useForm } from "react-hook-form";
-import { Rating } from '@material-ui/lab';
+import { useForm } from 'react-hook-form'
+import { Rating } from '@material-ui/lab'
 
-import { makeStyles } from '@material-ui/core/styles';
-//redux imports
-import { useSelector } from 'react-redux';
-import { db } from '../base';
-
+import { makeStyles } from '@material-ui/core/styles'
+import { useSelector } from 'react-redux'
+import { db } from '../base'
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
-        gridGap: theme.spacing(3),
-    },
-    button: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    title: {
-        paddingLeft: '20px'
-    },
-    rating: {
-        paddingLeft: '30px',
-        margin: '0 margin'
-    },
-    photo: {
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3)
+  },
+  button: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  title: {
+    paddingLeft: '20px'
+  },
+  rating: {
+    paddingLeft: '30px',
+    margin: '0 margin'
+  },
+  photo: {
+    paddingRight: '10px',
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    align: 'center',
+    justify: 'center'
+  },
+  outerCircle: {
+    borderRadius: '100%',
+    margin: '0 auto',
+    width: 170,
+    height: 170
+  },
+  bigAvatar: {
+    marginTop: '10px',
+    margin: '0 auto',
+    width: 150,
+    height: 150
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7)
+  }
+}))
 
-        paddingRight: '10px',
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        align: 'center',
-        justify: 'center',
+export default function MatchCompletion () {
+  const classes = useStyles()
 
-    },
-    outerCircle: {
-        borderRadius: '100%',
-        margin: '0 auto',
-        width: 170,
-        height: 170,
-    },
-    bigAvatar: {
-        marginTop: '10px',
-        margin: '0 auto',
-        width: 150,
-        height: 150,
-    },
-    large: {
-        width: theme.spacing(7),
-        height: theme.spacing(7),
-    }
+  const { register, errors, handleSubmit } = useForm()
 
-}));
+  const { selectedMatch, userData } = useSelector((state) => state.general)
 
+  const { requesterData, prop, ownerData } = selectedMatch
 
-export default function MatchCompletion() {
-    const classes = useStyles();
+  const partnerData =
+    userData.uid === requesterData.uid ? ownerData : requesterData
 
-    const { register, errors, handleSubmit } = useForm();
+  const archiveMatch = async () => {
+    const id = `${requesterData.uid}_${prop.id}_${ownerData.uid}`
+    // remove from primary collection
 
-    const { selectedMatch, userData } = useSelector(state => state.general);
+    const updates = { done: true }
 
-    const { requesterData, prop, ownerData } = selectedMatch;
+    updates[
+      userData.uid === requesterData.uid ? 'ownerScore' : 'requesterScore'
+    ] = score
 
-    const partnerData = userData.uid == requesterData.uid ? ownerData : requesterData;
+    await db
+      .collection('users')
+      .doc(userData.uid)
+      .collection('matches')
+      .doc(id)
+      .set(updates, { merge: true })
 
+    console.log('match archived')
+  }
+  const [score, setScore] = useState(0)
 
-    const archiveMatch = async () => {
-        const id = `${requesterData.uid}_${prop.id}_${ownerData.uid}`;
-        //remove from primary collection
+  const submitComment = async (values) => {
+    const { content } = values
 
-        const updates = {done:true}
+    // recoger la data del usuario
+    const { app } = await import('../base')
 
-        updates[userData.uid == requesterData.uid ? 'ownerScore' : 'requesterScore'] = score;
+    await app
+      .firestore()
+      .collection('users')
+      .doc(partnerData.uid)
+      .collection('comments')
+      .doc()
+      .set(
+        {
+          content: content,
+          name: userData.name,
+          lname: userData.lname,
+          photoUrl: userData.photoUrl
+        },
+        { merge: true }
+      )
+    archiveMatch()
+  }
 
-        await db.collection('users').doc(userData.uid).collection('matches').doc(id)
-        .set(updates,{merge:true});
+  const root = {
+    color: 'white !important',
+    padding: '20px 0',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  return (
+    <div style={root}>
+      <form onSubmit={handleSubmit(submitComment)}>
+        <Grid container spacing={3} direction="column" justify="flex-start">
+          <Grid item xs={12} sm={12} md={12}>
+            <Typography style={{ color: 'white' }} variant="h6" gutterBottom>
+              {'Califica tu experiencia con ' + partnerData.name}
+            </Typography>
+          </Grid>
 
-        console.log('match archived');
-    }
-    const [score,setScore] = useState(0);
+          <Grid
+            container
+            align="center"
+            justify="center"
+            item
+            xs={12}
+            sm={12}
+            md={12}
+          >
+            <div className={classes.outerCircle}>
+              <Avatar
+                src={partnerData.photoUrl}
+                className={classes.bigAvatar}
+              />
+            </div>
+          </Grid>
 
-    const submitComment = async values => {
+          <Grid item xs={12} sm={12} md={12}>
+            <Rating
+              onChange={(event, newScore) => {
+                console.log(newScore)
+                setScore(newScore)
+              }}
+              name="score"
+            />
+          </Grid>
 
-        const { content } = values;
+          <Grid item xs={12} sm={12} md={12}>
+            <TextField
+              inputRef={register({ required: true })}
+              name="content"
+              id="content"
+              helperText={errors.content && 'Debes escribir algo'}
+              variant="outlined"
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </Grid>
 
-        //recoger la data del usuario
-        const { app } = await import('../base');
-
-        await app.firestore().collection('users').doc(partnerData.uid).collection('comments').doc()
-            .set(
-                {
-                    content: content,
-                    name: userData.name,
-                    lname: userData.lname,
-                    photoUrl: userData.photoUrl,
-                }, { merge: true });
-        archiveMatch();
-    }
-
-    const root = {
-        color: 'white !important',
-        padding: '20px 0',
-        // width: '100vh',
-        display: 'flex',
-        flexDirection: 'column'
-    }
-    return (
-        <div style={root}>
-            <form onSubmit={handleSubmit(submitComment)}>
-                <Grid container spacing={3} direction="column" justify="flex-start">
-                    <Grid item xs={12} sm={12} md={12}>
-                        <Typography style={{ color: 'white' }} variant="h6" gutterBottom>
-                            {"Califica tu experiencia con " + partnerData.name}
-                        </Typography>
-                    </Grid>
-
-                    <Grid container align="center" justify="center" item xs={12} sm={12} md={12}>
-                        <div className={classes.outerCircle}>
-                            <Avatar src={partnerData.photoUrl} className={classes.bigAvatar} />
-                        </div>
-                    </Grid>
-
-                    <Grid item xs={12} sm={12} md={12}>
-                        <Rating onChange={(event, newScore) => { console.log(newScore); setScore(newScore);}} name="score" />
-                    </Grid>
-
-                    <Grid item xs={12} sm={12} md={12}>
-                        <TextField
-                            inputRef={register({ required: true })}
-                            name="content"
-                            id="content"
-                            helperText={errors.content && "Debes escribir algo"}
-                            variant="outlined"
-                            margin="normal"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12} sm={12} md={12}>
-                        <Button
-                            variant="outlined"
-                            type="submit"
-                        >Confirmar</Button>
-                    </Grid>
-                </Grid>
-            </form>
-        </div>
-    )
+          <Grid item xs={12} sm={12} md={12}>
+            <Button variant="outlined" type="submit">
+              Confirmar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  )
 }
