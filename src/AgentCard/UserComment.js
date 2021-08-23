f0c275fade9d25270c2ca8f091bd0d570e47ed89
Ronny Cajas <ronny1418@hotmail.com>
2021-08-22 21:55:51 -0500

    eslint, PropTypes

--- src/AgentCard/UserComment.js
+++ src/AgentCard/UserComment.js
@@ -1,37 +1,36 @@
 import React from 'react'
-import ListItem from '@material-ui/core/ListItem';
-import { makeStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types'
+import ListItem from '@material-ui/core/ListItem'
+import { makeStyles } from '@material-ui/core/styles'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        paddingLeft: 0,
-        paddingLight: 0,
-    },
-}));
+  root: {
+    paddingLeft: 0,
+    paddingLight: 0
+  }
+}))
 
-export default function UserComment(props) {
-    const classes = useStyles();
-    const { commentData } = props;
-    const {photoUrl, name, lname, content} = commentData
+export default function UserComment (props) {
+  const classes = useStyles()
+  const { commentData } = props
+  const { photoUrl, name, lname, content } = commentData
 
-    return (
-        <ListItem className={classes.root} alignItems="flex-start">
-            <ListItemAvatar>
-                <Avatar alt="Remy Sharp" src={photoUrl} />
-            </ListItemAvatar>
-            <ListItemText
-                primary={`${name} ${lname === null ? '' : lname}`}
-                secondary={
-                    <React.Fragment>
-                        {`${content}`}
-                    </React.Fragment>
-                }
-            />
-        </ListItem>
-    )
+  return (
+    <ListItem className={classes.root} alignItems="flex-start">
+      <ListItemAvatar>
+        <Avatar alt="Remy Sharp" src={photoUrl} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={`${name} ${lname === null ? '' : lname}`}
+        secondary={<React.Fragment>{`${content}`}</React.Fragment>}
+      />
+    </ListItem>
+  )
+}
+
+UserComment.propTypes = {
+  commentData: PropTypes.object.isRequired
 }
