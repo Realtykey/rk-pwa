74075b1303cce8db24e7e7de025aef2d777f0124
Ronny Cajas <ronny1418@hotmail.com>
2021-09-06 00:23:41 -0500

    linting

--- src/Property/PropertyCard/MoneyView.js
+++ src/Property/PropertyCard/MoneyView.js
@@ -1,62 +1,61 @@
-import React,{useEffect} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import Grid from '@material-ui/core/Grid';
-import Chip from '@material-ui/core/Chip';
+import Grid from '@material-ui/core/Grid'
+import Chip from '@material-ui/core/Chip'
 
 const chip = {
-    background: 'white',
-    color: '#292131',
-    fontWeight: 'bolder',
+  background: 'white',
+  color: '#292131',
+  fontWeight: 'bolder'
 }
 
 const highlighted = {
-    background: 'white',
-    color: 'red',
-    fontWeight: 'bolder',
+  background: 'white',
+  color: 'red',
+  fontWeight: 'bolder'
 }
 
-export default function MoneyView({propData}) {
-    useEffect(
-    ()=>{console.log('precio: ',propData.price)}
-    ,[]
-    );
-    const root = {
-        margin: '20px auto'
-    }
-    return (
-        <div style={root}>
-            <Grid
-                spacing={1}
-                container
-                direction="row"
-                alignItems="center"
-            >
-                <Grid item>
-                    <Chip
-                        style={chip}
-                        label={propData.comission? "Precio $ "+propData.price:"Presupuesto $ " + propData.price}
-                    />
-                </Grid>
-                {propData.comission &&<>
-                <Grid item>
-                    <Chip
-                        style={chip}
-                        label={" Te Comparto "+Number.parseFloat(propData.comission.percent) + "%"}
-                    />
-                </Grid>
-                
-                <Grid item>
-                    <Chip
-                        style={highlighted}
-                        variant="outlined"
-                        label={"Tu Ganas $ " + Number.parseFloat(propData.comission.value)}
-                    />
-                </Grid> </>
-                }
+export default function MoneyView (props) {
+  const { propData } = props
+  const { comission, price } = propData
+  const { percent, value } = comission ?? {}
 
+  const root = {
+    margin: '20px auto'
+  }
+  return (
+    <div style={root}>
+      <Grid spacing={1} container direction="row" alignItems="center">
+        <Grid item>
+          <Chip
+            style={chip}
+            label={comission ? 'Precio $ ' + price : 'Presupuesto $ ' + price}
+          />
+        </Grid>
+        {comission && (
+          <>
+            <Grid item>
+              <Chip
+                style={chip}
+                label={' Te Comparto ' + Number.parseFloat(percent) + '%'}
+              />
             </Grid>
+            <Grid item>
+              <Chip
+                style={highlighted}
+                variant="outlined"
+                label={'Tu Ganas $ ' + Number.parseFloat(value)}
+              />
+            </Grid>{' '}
+          </>
+        )}
+      </Grid>
+    </div>
+  )
+}
 
-
-        </div>
-    )
+MoneyView.propTypes = {
+  propData: PropTypes.object,
+  price: PropTypes.number
 }
--- src/Property/PropertyForm/ImagesPreview.js
+++ src/Property/PropertyForm/ImagesPreview.js
@@ -1,67 +1,63 @@
-import React, { useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import PropTypes from 'prop-types'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
-    overflow: 'hidden',
+    overflow: 'hidden'
   },
   gridList: {
-    display: 'flex',
+    display: 'flex'
   },
   icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+    color: 'rgba(255, 255, 255, 0.54)'
   },
   shadedImg: {
-    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+    boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)'
   },
 
   shadedTile: {
-    transition: "0.3s",
-    boxShadow: "0 16px 70px  rgba(0,0,0,0.3)"
-
+    transition: '0.3s',
+    boxShadow: '0 16px 70px  rgba(0,0,0,0.3)'
   }
-}));
+}))
 
-export default function ImagesPreview({ urls }) {
-  const classes = useStyles();
-  useEffect(() => {
-    console.log('images : ')
-    console.log(urls)
-
-  }, [])
+export default function ImagesPreview (props) {
+  const { urls } = props
+  const classes = useStyles()
 
   return (
-
     <div align="left" className={classes.root}>
       <GridList className={classes.gridList} cols={3}>
-
-        {urls && urls.map((url, index) => {
-
-          return (urls.length > 1) ? <GridListTile
-            key={(url)}
-            cols={(index == 0 || index == 1) ? 1.5 : 1}
-            rows={(index == 0 || index == 1) ? 1.3 : 0.7}
-            className={classes.shadedTile}
-          >
-            <img alt="prop tile" src={url} />
-          </GridListTile> : <GridListTile
-            key={(url)}
-            cols={3}
-            rows={3}
-          >
-              <img alt="prop tile" className={classes.shadedImg} src={url} />
-            </GridListTile>
-
-        })}
-
+        {urls &&
+          urls.map((url, index) => {
+            return urls.length > 1
+              ? (
+              <GridListTile
+                key={url}
+                cols={index === 0 || index === 1 ? 1.5 : 1}
+                rows={index === 0 || index === 1 ? 1.3 : 0.7}
+                className={classes.shadedTile}
+              >
+                <img alt="prop tile" src={url} />
+              </GridListTile>
+                )
+              : (
+              <GridListTile key={url} cols={3} rows={3}>
+                <img alt="prop tile" className={classes.shadedImg} src={url} />
+              </GridListTile>
+                )
+          })}
       </GridList>
     </div>
+  )
+}
 
-
-  );
+ImagesPreview.propTypes = {
+  urls: PropTypes.array
 }
