d2d0b344a1fbc51a7973f80c20defe3b6e96f8e0
Ronny Cajas <ronny1418@hotmail.com>
2022-05-02 04:56:29 -0500

    code cleaning - eslint

--- src/Property/Features.js
+++ src/Property/Features.js
@@ -1,63 +1,69 @@
 import React from 'react'
-import Chip from '@material-ui/core/Chip';
-import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types'
+import Chip from '@material-ui/core/Chip'
+import Grid from '@material-ui/core/Grid'
 
-//prop info icons
-import HotelIcon from '@material-ui/icons/Hotel';
-import BathtubIcon from '@material-ui/icons/Bathtub';
-import AspectRatioIcon from '@material-ui/icons/AspectRatio';
-import DriveEtaIcon from '@material-ui/icons/DriveEta';
+import HotelIcon from '@material-ui/icons/Hotel'
+import BathtubIcon from '@material-ui/icons/Bathtub'
+import AspectRatioIcon from '@material-ui/icons/AspectRatio'
+import DriveEtaIcon from '@material-ui/icons/DriveEta'
 
-export default function Features({ propData }) {
+export default function Features (props) {
+  const { propData } = props
+  const { dormitories, bathrooms, area, parkings } = propData
+  const root = {
+    margin: '20px auto'
+  }
+  return (
+    <div style={root}>
+      <Grid spacing={1} container direction="row" alignItems="center">
+        <Grid sm={3} xs={4} align="center" item>
+          {dormitories && (
+            <Chip
+              variant="outlined"
+              icon={<HotelIcon />}
+              label={' Dormitorios ' + dormitories}
+            ></Chip>
+          )}
+        </Grid>
 
-    const root = {
-        margin: '20px auto'
-    }
-    return (
-        <div style={root}>
-            <Grid
-                spacing={1}
-                container
-                direction="row"
-                alignItems="center"
-            >
+        <Grid sm={3} xs={4} align="center" item>
+          {bathrooms && (
+            <Chip
+              variant="outlined"
+              icon={<BathtubIcon />}
+              label={' Baños ' + bathrooms}
+            />
+          )}
+        </Grid>
 
-                <Grid sm={3} xs={4} align="center" item>
-                    {propData.dormitories && <Chip
-                        variant="outlined"
-                        icon={<HotelIcon />}
-                        label={" Dormitorios " + propData.dormitories}
-                    >
-                    </Chip>}
+        <Grid sm={3} xs={4} align="center" item>
+          <Chip
+            variant="outlined"
+            icon={<AspectRatioIcon />}
+            label={' Area ' + area + ' m²'}
+          />
+        </Grid>
 
-                </Grid>
+        <Grid sm={3} xs={12} align="center" item>
+          {parkings && (
+            <Chip
+              variant="outlined"
+              icon={<DriveEtaIcon />}
+              label={' Parqueaderos ' + parkings}
+            />
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
 
-                <Grid sm={3} xs={4} align="center" item>
-                    {propData.bathrooms && <Chip
-                        variant="outlined"
-                        icon={<BathtubIcon />}
-                        label={" Baños " + propData.bathrooms}
-                    />}
-                </Grid>
-
-                <Grid sm={3} xs={4} align="center" item>
-                    <Chip
-                        variant="outlined"
-                        icon={<AspectRatioIcon />}
-                        label={" Area " + propData.area + " m²"}
-                    />
-                </Grid>
-
-                <Grid sm={3} xs={12} align="center" item>
-                    {propData.parkings && <Chip
-                        variant="outlined"
-                        icon={<DriveEtaIcon />}
-                        label={" Parqueaderos " + propData.parkings}
-                    />}
-                </Grid>
-
-            </Grid>
-
-        </div>
-    )
+Features.propTypes = {
+  propData: PropTypes.shape({
+    dormitories: PropTypes.number,
+    bathrooms: PropTypes.number,
+    area: PropTypes.number,
+    parkings: PropTypes.number
+  })
 }
